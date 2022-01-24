578fdbca5e68d8ab8777bc00fb132231cfd16c74
Ming Li <limiang929@gmail.com>
2022-01-24 11:55:08 +0800

    make test files

---
+++ tests/testthat/test-getBaseModificationDf.R
@@ -0,0 +1,14 @@
+library(BMplot)
+
+context("the return of getBaseModificationDf()")
+
+test_that("getBaseModificationDf() return a data.frame", {
+
+  skip_if_not_installed("BSgenome.Athaliana.TAIR.TAIR9")
+
+  expect_true(is(getBaseModificationDf(region = A_thaliana_dmR[1,],
+                                       BSseq = A_thaliana_BSobj,
+                                       BSgenome = BSgenome.Athaliana.TAIR.TAIR9::Athaliana,
+                                       motif = c("CHH","CHG"),
+                                       base = "C"), "data.frame"))
+})
